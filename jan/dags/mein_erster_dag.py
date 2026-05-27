86425f28054b6f70156fb68ee67608af21e9834a
Jan <jan.oesterle@huk-coburg.de>
2026-05-27 09:20:51 +0000

    task 1

---
+++ jan/dags/mein_erster_dag.py
@@ -0,0 +1,25 @@
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+@task(task_id="process_task")
+def process():
+    print("Processing...")
+
+with DAG(
+     dag_id="mein_erster_dag",
+     start_date=datetime.datetime(2024, 1, 1),
+     schedule="@daily",
+     catchup=False
+ ) as dag:
+    start_task = BashOperator(
+        task_id="start_task",
+        bash_command='echo "Start"'
+    )
+
+    process_task = process()
+
+    end_task = BashOperator(
+        task_id="end_task",
+        bash_command='echo "End"'
+    )
+     
