ee1bc6d07cc0e7466dc2a8df16dbe09654959cf2
Jan <jan.oesterle@huk-coburg.de>
2026-05-27 11:48:06 +0000

    p07

--- participants/p07/mein_zweiter_dag.py
+++ participants/p07/mein_zweiter_dag.py
@@ -13,24 +13,22 @@ import pendulum
     catchup=True,
     tags=["p07", "workshop", "task2"],
 )
-def mein_erster_dag():
+def mein_zweiter_dag():
 
     @task(task_id="produce_data", do_xcom_push=True)
     def produce_data():
         return {"user": "ish", "score": 42}
 
     @task(task_id="consume_data", do_xcom_push=True)
-    def consume_data():
-        raise Exception
+    def consume_data(**context):
+        user = context["ti"].xcom_pull(task_ids="produce_data", key="user")
+        score = context["ti"].xcom_pull(task_ids="produce_data", key="score")
+        print(f"User {user} has score {score}")
 
-    @task(task_id="log_date")
-    def log_date():
-        raise Exception
-
+    log_date = BashOperator(
+        task_id="log_date",
+        bash_command='echo "Run {{ logical_date | ds }}"',
+    )
     produce_data() >> consume_data() >> log_date()
 
-
-
 dag = mein_zweiter_dag()
-
-
