1819bae4da37b01159bc960123aabe528698e61c
leif <l@.huk.com>
2026-05-27 09:22:06 +0000

    p06: Mein erster DAG

---
+++ participants/p06/mein_erster_dag.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="p06_hello_world",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["workshop", "p06","leifcd"],
+) as dag:
+    BashOperator(
+        task_id="hello",
+        bash_command="echo 'Hallo Airflow!'",
+    )
