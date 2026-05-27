3d1fab8292e892fe11109e706f55b4f16acb20ae
Andre <andre@example.com>
2026-05-27 12:00:27 +0000

    p05: 04

--- participants/p05/dags/orchestration_demo_dag.py
+++ participants/p05/dags/orchestration_demo_dag.py
@@ -1,15 +1,36 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.bash import BashOperator
+from __future__ import annotations
 
-with DAG(
-    dag_id="p05_dag_04_a",
-    start_date=datetime(2026, 5, 27),
-    schedule='@hourly',
+import pendulum
+from airflow.decorators import dag, task
+from airflow.operators.bash import BashOperator
+from janis.libs.calculator import add, subtract, multiply, divide
+
+@dag(
+    dag_id="p05_dag_04",
+    start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),  # timezone-aware
+    schedule="@daily",
     catchup=False,
-    tags=["workshop", "p05"],
-) as dag:
-    BashOperator(
-        task_id="dag_04_a",
-        bash_command="echo 'Hallo Airflow aus Aufgabe 4 DAG A!'",
+    tags=["workshop", "basics"],
+)
+def mein_erster_dag():
+    @task
+    def print_hello():
+        print("Hello Airflow")
+
+    show_date = BashOperator(
+        task_id="p05_show_date",
+        bash_command='echo "Logical execution date is {{ ds }}"',
     )
+
+    @task
+    def addiere():
+        print(add(1,2))
+
+    @task
+    def final_step():
+        print("Pipeline complete")
+
+    print_hello() >> show_date >> addiere() >> final_step()
+
+
+dag = mein_erster_dag()
