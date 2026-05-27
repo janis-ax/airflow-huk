490a0e478e0f62fd2a0e50e4cad708ec013e5d5a
janis-ax <janisax@icloud.com>
2026-05-27 13:29:21 +0200

    add libs

---
+++ janis/janis_02.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import pendulum
+from airflow.decorators import dag, task
+from airflow.operators.bash import BashOperator
+from ..libs.calculator import add, subtract, multiply, divide
+
+@dag(
+    dag_id="janis_calculator",
+    start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),  # timezone-aware
+    schedule="@daily",
+    catchup=False,
+    tags=["workshop", "basics"],
+)
+def mein_erster_dag():
+    @task
+    def print_hello():
+        print("Hello Airflow")
+
+    show_date = BashOperator(
+        task_id="show_date",
+        bash_command='echo "Logical execution date is {{ ds }}"',
+    )
+
+    @task
+    def addiere():
+        print(add(1,2))
+
+    @task
+    def final_step():
+        print("Pipeline complete")
+
+    print_hello() >> show_date >> addiere() >> final_step()
+
+
+dag = mein_erster_dag()
