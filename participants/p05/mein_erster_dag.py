9eeda272d1f625d8936e70191b0834b84111b567
Andre <andre@example.com>
2026-05-27 11:15:30 +0000

    p05: 03-01

--- participants/p05/mein_erster_dag.py
+++ participants/p05/mein_erster_dag.py
@@ -4,8 +4,8 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="p05_hello_world",
-    start_date=datetime(2026, 1, 1),
-    schedule=None,
+    start_date=datetime(2027, 5, 20),
+    schedule=@Hourly,
     catchup=False,
     tags=["workshop", "p05"],
 ) as dag:
