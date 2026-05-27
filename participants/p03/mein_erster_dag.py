97e05a8745ae977776f7f1eb03184da24ac40a15
ph <ph>
2026-05-27 09:22:49 +0000

    p03 commit

--- participants/p03/mein_erster_dag.py
+++ participants/p03/mein_erster_dag.py
@@ -4,7 +4,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 
 with DAG(
-    dag_id="Hello p03",
+    dag_id="hello-p03",
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     schedule="@hourly",
     catchup=False,
