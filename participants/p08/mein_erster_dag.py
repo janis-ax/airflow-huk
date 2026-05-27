b94c3efbbe6d5b6b117bdfeb278b0bb9ace5fabb
Juergen <juergen.wich@huk-coburg.de>
2026-05-27 09:17:17 +0000

    p08: juergens dag

---
+++ participants/p08/mein_erster_dag.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="p01_hello_world",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["workshop", "p08"],
+) as dag:
+    BashOperator(
+        task_id="hello",
+        bash_command="echo 'Hallo Juergen!'",
+    )
