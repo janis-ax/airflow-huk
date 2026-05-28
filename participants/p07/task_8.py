40efa3b4600acdde335c8b1855fbba5e442d1916
Jan <jan.oesterle@huk-coburg.de>
2026-05-28 08:17:53 +0000

    p07

--- participants/p07/task_8.py
+++ participants/p07/task_8.py
@@ -22,7 +22,7 @@ def mein_zweiter_dag():
 
     build_path = BashOperator(
         task_id="build_path_p07",
-        bash_command='echo "/data/{{ logical_date | ds }}/{{ ti.xcom_pull(task_ids='read_country_p07') }}/input.csv"'
+        bash_command='echo "/data/{{ logical_date | ds }}/{{ ti.xcom_pull(task_ids=\'read_country_p07\') }}/input.csv"'
     )
 
     final_log = BashOperator(
